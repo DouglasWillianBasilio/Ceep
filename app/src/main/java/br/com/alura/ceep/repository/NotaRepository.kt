ac236f024fdf8eedf49d93f8c8a6921a6a203b8e
Douglas1301 <125305607+DouglasWillianBasilio@users.noreply.github.com>
2023-12-28 05:49:24 -0300

    Integrando com a WEB API, salvando notas na api e criando flags de sincronização

--- app/src/main/java/br/com/alura/ceep/repository/NotaRepository.kt
+++ app/src/main/java/br/com/alura/ceep/repository/NotaRepository.kt
@@ -5,17 +5,32 @@ import br.com.alura.ceep.model.Nota
 import br.com.alura.ceep.webclient.NotaWebClient
 import kotlinx.coroutines.flow.Flow
 
-class NotaRepository(private val dao: NotaDao,
-private val webClient: NotaWebClient) {
+class NotaRepository(
+    private val dao: NotaDao,
+    private val webClient: NotaWebClient
+) {
 
     fun buscaTodas(): Flow<List<Nota>> {
-       return dao.buscaTodas()
+        return dao.buscaTodas()
     }
 
     suspend fun atualizaTodas() {
         webClient.buscaTodas()?.let { notas ->
             dao.salva(notas)
         }
+    }
+
+    fun buscaPorId(id: String): Flow<Nota> {
+        return dao.buscaPorId(id)
+    }
+
+    suspend fun remove(id: String) {
+        dao.remove(id)
+    }
+
+    suspend fun salva(nota: Nota) {
+        dao.salva(nota)
+        webClient.salva(nota)
     }
 
 }
